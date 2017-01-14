2c16640e181a4a011aba7419dfcab9c8228cb5d8
JR HARI NANDHA <jrhn1596@gmail.com>
2017-01-14 13:48:54 +0530

    QR Scanner
    A mock app using mobile vision API to scan QR codes.

---
+++ app/src/main/java/com/reality/escape/qrscanner/MainActivity.java
@@ -0,0 +1,82 @@
+package com.reality.escape.qrscanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView scanText;
+    SurfaceView cameraView;
+    CameraSource cameraSource;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraView = (SurfaceView) findViewById(R.id.camera_view);
+        scanText = (TextView) findViewById(R.id.code_info);
+
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(480, 480)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>(){
+
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodeSparseArray = detections.getDetectedItems();
+
+                if(barcodeSparseArray.size()!=0){
+                    scanText.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            scanText.setText(barcodeSparseArray.valueAt(0).displayValue);
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
